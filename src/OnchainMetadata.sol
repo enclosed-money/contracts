222e34e1f8bb62198aaf32e999aeff7b9e3a4819
vincanger <70215737+vincanger@users.noreply.github.com>
2022-09-14 11:58:41 +0200

    trying out Base64 encoded SVG URI 🤷‍♂️

--- src/OnchainMetadata.sol
+++ src/OnchainMetadata.sol
@@ -1,10 +1,68 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.13;
 
+import './Bill.sol';
 import './interfaces/IMetadata.sol';
+import 'openzeppelin-contracts/contracts/utils/Base64.sol';
+import 'openzeppelin-contracts/contracts/utils/Strings.sol';
+
+import 'forge-std/console.sol';
 
 contract OnchainMetadata is IMetadata {
-    function uri(uint256) public view virtual override returns (string memory) {
-        return '<svg>';
+    using Strings for uint256;
+    
+    struct Metadata {
+        string symbol;
+        string name;
+        address erc20;
+        uint96 value;
+        uint8 decimals;
+    }
+
+    Bill public bill;
+
+    constructor(Bill _bill) {
+        bill = _bill;
+    }
+
+    function uri(uint256 id) public view virtual override returns (string memory) {
+        address erc20 = address(uint160(id >> 96));
+        // TODO: refactor hardcoded value
+        uint96 value = uint96(id)*10**18; 
+
+        // TODO: check registry and return proper stuff
+        // just hardcoded for now
+        Metadata memory _metadata = Metadata({
+            symbol: 'USDC',
+            name: 'USD Coin',
+            erc20: erc20,
+            value: value,
+            decimals: 18
+        });
+
+        string memory _renderedMetadata = Base64.encode(bytes(bill.render(_metadata.symbol, _metadata.name, _metadata.erc20, _metadata.value, _metadata.decimals)));
+
+        string memory json = Base64.encode(
+        bytes(
+            string(
+                abi.encodePacked(
+                    '{"name": "ENCLOSED.MONEY - #',
+                    id.toString(),
+                    '", "description": "Turn Your Magic Internet Money Into an NFT -- Because, Why Not?!", "image": "data:image/svg+xml;base64,',
+                    _renderedMetadata,
+                    '"}'
+                )
+            )
+        )
+        );
+
+        // TODO: remove
+        console.log(string(
+            abi.encodePacked("data:application/json;base64,", json)
+        ));
+
+        return string(
+            abi.encodePacked("data:application/json;base64,", json)
+        );
     }
 }
--- test/OnchainMetadata.t.sol
+++ test/OnchainMetadata.t.sol
@@ -3,16 +3,17 @@ pragma solidity ^0.8.13;
 
 import 'src/OnchainMetadata.sol';
 import 'forge-std/Test.sol';
+import '../src/Bill.sol';
 
 contract OnchainMetadataTest is Test {
     OnchainMetadata meta;
 
     function setUp() public {
-        meta = new OnchainMetadata();
+        meta = new OnchainMetadata(new Bill());
     }
 
     function testRender() public {
-        string memory uri = meta.uri(1);
+        string memory uri = meta.uri(0x95ad61b0a150d79219dcf64e1e6cc01f0b64c4ce000000000000000000000001);
 
         string[] memory inputs = new string[](3);
         inputs[0] = 'node';
