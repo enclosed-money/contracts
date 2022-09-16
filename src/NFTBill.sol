206e3fe1d116109b72c3c88a58459d69efe814c2
汪帅 <1113241948@qq.com>
2022-09-16 10:16:35 +0800

    consolidate the functionality

--- src/NFTBill.sol
+++ src/NFTBill.sol
@@ -40,24 +40,19 @@ contract NFTBill is ERC1155 {
     }
 
     function withdraw(uint256 id) external {
-        _burn(msg.sender, id, 1);
-        address erc20 = address(uint160(id >> 96));
-        uint96 value = uint96(id);
-
-        if (erc20 == address(0)) {
-            (bool ok, bytes memory data) = msg.sender.call{value: value}('');
-            require(ok, string(data));
-        } else {
-            ERC20(erc20).transfer(msg.sender, value);
-        }
+        _withdrawTo(msg.sender, id);
     }
     
-    // withdraw token to specified address, save gas fee on calling NFT transfer method
     function withdrawTo(address to, uint256 id) external {
+        _withdrawTo(to, id);
+    }
+
+    // withdraw token to specified address, save gas fee on calling NFT transfer method
+    function _withdrawTo(address to, uint256 id) internal {
         _burn(msg.sender, id, 1);
         address erc20 = address(uint160(id >> 96));
         uint96 value = uint96(id);
-
+        
         if (erc20 == address(0)) {
             (bool ok, bytes memory data) = to.call{value: value}('');
             require(ok, string(data));
